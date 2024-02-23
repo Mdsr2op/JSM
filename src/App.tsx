81b1b58bb6ada8167b33a6805ffe5583e550f858
Mdsr2op <mdsrmfkhan@gmail.com>
2024-02-23 09:02:38 +0500

    Wrote the structure code for social media app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="h1-bold">hello world</h1>
+    </>
+  );
+}
+
+export default App;
