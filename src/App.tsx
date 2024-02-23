fc61611571825827278077137c6ec50fb28079c3
Mdsr2op <mdsrmfkhan@gmail.com>
2024-02-23 10:51:50 +0500

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <>
-      <h1 className="h1-bold">hello world</h1>
-    </>
+    <main className="h-screen flex">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="./sign-in" element={<SigninForm />} />
+          <Route path="./sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* protected routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   );
-}
+};
 
 export default App;
